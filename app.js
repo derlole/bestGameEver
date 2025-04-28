a6317ed9a3752a00c7863cdda3e84e69a9bde4ed
derlole <122916573+derlole@users.noreply.github.com>
2025-04-28 17:32:04 +0000

    jeet

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const PORT = 5000 || process.env.PORT
 
 connectDB()
 
-app.use('/SERU_TPHEU', express.static('public'));
+app.use(express.static('public'));
 
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
 app.set('layout', './layouts/main')
 
 
-app.use('/SERU_TPHEU', require('./server/routes/main'))
+app.use('/', require('./server/routes/main'))
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`)
